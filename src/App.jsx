8f7f74ab4871b528112a3d28e562145b2a415f78
inozavou <inozavou@gmail.com>
2025-02-17 20:08:15 +0200

    Initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,181 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import companyData from "./data.json";
+
+function App() {
+  const [tagName, setTagName] = useState([]);
+  const [searchBarVisible, setSearchBarVisible] = useState(false);
+
+  return (
+    <div className="App">
+      <div className="app-background">
+        <div className="main-content">
+          <Header />
+          <SearchBar
+            tagName={tagName}
+            setTagName={setTagName}
+            searchBarVisible={searchBarVisible}
+            setSearchBarVisible={setSearchBarVisible}
+          />
+          <Job
+            companyData={companyData}
+            tagName={tagName}
+            searchBarVisible={searchBarVisible}
+            setSearchBarVisible={setSearchBarVisible}
+            setTagName={setTagName}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="header-shapes"
+          src="./images/bg-header-desktop.svg"
+          alt="header-shapes"
+        />
+      </div>
+    </div>
+  );
+}
+
+function Job({ companyData, tagName, setTagName, setSearchBarVisible }) {
+  function handleClick(clickedTag) {
+    setSearchBarVisible(true);
+
+    if (tagName.includes(clickedTag)) return;
+
+    setTagName((prevTagName) => [...prevTagName, clickedTag]);
+  }
+
+  const filteredCompanies =
+    tagName.length > 0
+      ? companyData.filter((company) => {
+          const companyTags = [
+            company.role,
+            company.level,
+            ...company.languages,
+            ...company.tools,
+          ];
+          return tagName.every((tag) => companyTags.includes(tag));
+        })
+      : companyData;
+
+  return (
+    <div className="jobs-container">
+      {filteredCompanies.map((company) => (
+        <div className="job-container" key={company.id}>
+          <div className="logo-and-details">
+            <div className="company-logo">
+              <img
+                className="company-logo-image"
+                src={company.logo}
+                alt={company.id}
+              />
+            </div>
+
+            <div className="job-position-details">
+              <div className="company-new-featured-container">
+                <p className="company-name">{company.company}</p>
+                {company.new && <p className="new">NEW!</p>}
+                {company.featured && <p className="featured">FEATURED</p>}
+              </div>
+              <p className="job-position">{company.position}</p>
+              <ul className="bottom-details">
+                <li>{company.postedAt}</li>
+                <li>{company.contract}</li>
+                <li>{company.location}</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="tags">
+            <p
+              className="tag"
+              onClick={() => {
+                handleClick(company.role);
+              }}
+            >
+              {company.role}
+            </p>
+            <p
+              className="tag"
+              onClick={() => {
+                handleClick(company.level);
+              }}
+            >
+              {company.level}
+            </p>
+            {company.languages.map((language, index) => (
+              <p
+                className="tag"
+                key={index}
+                onClick={() => {
+                  handleClick(language);
+                }}
+              >
+                {language}
+              </p>
+            ))}
+            {company.tools.length > 0 &&
+              company.tools.map((tool) => (
+                <div
+                  className="tag"
+                  onClick={() => {
+                    handleClick(tool);
+                  }}
+                >
+                  <p>{tool}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default App;
+
+function SearchBar({
+  tagName,
+  setTagName,
+  setSearchBarVisible,
+  searchBarVisible,
+}) {
+  function handleDeleteTag(element) {
+    setTagName((prevTags) => prevTags.filter((tag) => tag !== element));
+  }
+
+  return (
+    searchBarVisible && (
+      <div className="search-bar">
+        {tagName?.length > 0 &&
+          tagName.map((element, index) => (
+            <div className="searchBar-tags" key={index}>
+              <div className="tagName">{element}</div>
+              <button
+                className="delete-tag"
+                onClick={() => handleDeleteTag(element)}
+              >
+                &#x2716;
+              </button>
+            </div>
+          ))}
+        <button
+          className="clear-button"
+          onClick={() => {
+            setTagName([]);
+            setSearchBarVisible(false);
+          }}
+        >
+          Clear
+        </button>
+      </div>
+    )
+  );
+}
---
+++ src/Job.jsx
@@ -0,0 +1,5 @@
+function Job() {
+  return <div></div>;
+}
+
+export default Job;
